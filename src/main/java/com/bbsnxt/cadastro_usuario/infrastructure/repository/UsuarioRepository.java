c204dd2769e991d3dcfcfa75bed0d8504ecc907f
Felipe <felipebbarbosasantos@gmail.com>
2025-08-10 21:44:08 -0300

    feat: repository UsuarioRepository

--- src/main/java/com/bbsnxt/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/bbsnxt/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -1,7 +1,15 @@
 package com.bbsnxt.cadastro_usuario.infrastructure.repository;
 
 import com.bbsnxt.cadastro_usuario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
 }
