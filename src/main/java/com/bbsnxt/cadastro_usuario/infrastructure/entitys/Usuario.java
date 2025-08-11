95ccffa25a6acb83650b1aa5942fb9673c98deea
Felipe <felipebbarbosasantos@gmail.com>
2025-08-10 21:19:32 -0300

    feat: entity Usuario

---
+++ src/main/java/com/bbsnxt/cadastro_usuario/infrastructure/entitys/Usuario.java
@@ -0,0 +1,25 @@
+package com.bbsnxt.cadastro_usuario.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+}
